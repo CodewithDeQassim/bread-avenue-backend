95a1f0374013f5f0fe440460684b509f5696d587
Alqassim Jalloh <alqassimjalloh535@gmail.com>
2026-01-04 18:59:12 +0000

    Refactored order entity structure

--- app/order/models.py
+++ app/order/models.py
@@ -5,11 +5,6 @@ from ..database import Base
 class Customer(Base):
     __tablename__ = "customers"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-    phone = Column(String, nullable=True)
-    address = Column(String, nullable=True)
 
 class Order(Base):
     __tablename__ = "orders"
